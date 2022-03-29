44c31e36e1b5618963d9be7130ad4e3d28f73488
nsmeltz <smeltznatalie@gmail.com>
2022-03-29 13:44:55 -0500

    Del 1 complete

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,9 +1,19 @@
 # import dependencies
+library(tidyverse)
 library(dplyr)
+library(ggplot2)
 
+# -------------------------- MechaCar prototypes ----------------------------
 # Read in MechaCar Data
 mechacar <- read.csv(file = 'Data/MechaCar_mpg.csv')
-headers <- names(mechacar)
 
 # Linear Regression
-lm()
+model <- lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD , data = mechacar)
+
+# determine p-value and r^2
+summary(model)
+
+# ---------------------- MechaCar Suspension Coils ------------------------
+# read in dataset
+suspension <- read.csv(file = 'Data/Suspension_Coil.csv')
+
